8c1106b29134d93cbc45c4516ccd874db5d79ad7
Toshihiko Ichida <dogatana@gmail.com>
2021-08-07 18:43:30 +0900

    format by black

--- docx2md.py
+++ docx2md.py
@@ -9,8 +9,10 @@ import zipfile
 
 from lxml import etree
 
+
 class InvalidDocxfileError(RuntimeError):
     pass
+
 
 class DocxFile:
     def __init__(self, filename):
@@ -22,7 +24,7 @@ class DocxFile:
         self.docx = zipfile.ZipFile(filename)
         if "word/document.xml" not in self.namelist():
             raise InvalidDocxfileError()
-    
+
     def document(self):
         return self.docx.read("word/document.xml")
 
@@ -35,7 +37,7 @@ class DocxFile:
             os.makedirs(target_dir)
 
         for image in images:
-            file = os.path.join(target_dir, image[len("word/media/"):])
+            file = os.path.join(target_dir, image[len("word/media/") :])
             with open(file, "wb") as f:
                 f.write(self.read(image))
 
@@ -44,21 +46,23 @@ class DocxFile:
 
     def namelist(self):
         return self.docx.namelist()
-    
+
     def read(self, filename):
         if filename not in self.namelist():
             return None
         return self.docx.read(filename)
-    
+
+
 class NamespaceResolver:
     def __init__(self, nsmap):
         self.map = {}
         for k, v in nsmap.items():
             self.map[v] = k
-    
+
     def tag(self, name):
         m = re.search(r"\{(.*?)\}(.*)", name)
         return f"{self.map[m.group(1)]}:{m.group(2)}"
+
 
 def parse_docx(file):
     docx = DocxFile(file)
@@ -76,36 +80,45 @@ def parse_docx(file):
     print("-" * 10, text, "-" * 10, sep="\n")
     open("out.md", "w", encoding="utf-8").write(text)
 
+
 def save_xml(file, text):
     # save for debug
     xml_file = os.path.splitext(file)[0] + ".xml"
     if os.path.exists(xml_file):
         return
     open(xml_file, "wb").write(text)
-    print("save to", xml_file )
+    print("save to", xml_file)
+
 
 def strip_ns_prefix(tree):
-    #xpath query for selecting all element nodes in namespace
+    # xpath query for selecting all element nodes in namespace
     query = "descendant-or-self::*[namespace-uri()!='']"
-    #for each element returned by the above xpath query...
+    # for each element returned by the above xpath query...
     for element in tree.xpath(query):
-        #replace element name with its local name
+        # replace element name with its local name
         element.tag = etree.QName(element).localname
     return tree
 
+
 STOP_PARSING = [
-    "pPr", "rPr", "sectPr", "pStyle", "wPr", "numPr", "ind", "shapetype", "tab"
+    "pPr",
+    "rPr",
+    "sectPr",
+    "pStyle",
+    "wPr",
+    "numPr",
+    "ind",
+    "shapetype",
+    "tab",
 ]
-NOT_PROCESS = [
-    "shape", "txbxContent"
-]
+NOT_PROCESS = ["shape", "txbxContent"]
 
 
 def parse_node(of, node, depth):
     if node.tag in STOP_PARSING:
         return
     for child in node.getchildren():
-        if  child is None:
+        if child is None:
             print("** none")
             continue
         tag = child.tag
@@ -116,7 +129,7 @@ def parse_node(of, node, depth):
         elif tag == "r":
             parse_node(of, child, depth + 1)
         elif tag == "br":
-            attr = ' class="page"' 
+            attr = ' class="page"'
             if get_attr(child, "type") == "page":
                 print('\n<div class="break"></div>\n', file=of)
             else:
@@ -143,8 +156,9 @@ def parse_node(of, node, depth):
             print("</td>", file=of)
         else:
             if tag not in NOT_PROCESS:
-                print("#", tag) 
+                print("#", tag)
             parse_node(of, child, depth + 1)
+
 
 def parse_tc(of, node, depth):
     sub_of = io.StringIO()
@@ -153,7 +167,10 @@ def parse_tc(of, node, depth):
     text = re.sub(r"\n+", "<br>", sub_text)
     print(text, end="", file=of)
 
+
 in_list = False
+
+
 def parse_p(of, node, depth):
     global in_list
     """ parse paragraph """
@@ -187,17 +204,20 @@ def parse_p(of, node, depth):
     elif style.isdigit():
         print("#" * (int(style)), sub_text, file=of)
     else:
-        raise RuntimeError("pStyle: " + style) 
+        raise RuntimeError("pStyle: " + style)
+
 
 def get_first_element(tree, xpath):
     tags = tree.xpath(xpath)
     return tags[0] if len(tags) > 0 else None
-    
+
+
 def get_attr(tag, name):
     for key, value in tag.attrib.items():
         if key.endswith("}" + name):
             return value
     return None
+
 
 def parse_drawing(of, node):
     tag = get_first_element(node, ".//cNvPr")
@@ -207,17 +227,18 @@ def parse_drawing(of, node):
     else:
         print(f"*** no pictures")
 
+
 def decode_shape(gfxdata, file):
     decoded = base64.b64decode(gfxdata.replace("&#xA;", "\n"))
     open(file, "wb").write(decoded)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("usage: python docx2md <word.docx>")
         exit()
     file = sys.argv[1]
-    if not os.path.exists(file) :
+    if not os.path.exists(file):
         print(file, "does not exit.")
         exit()
     parse_docx(file)
-
