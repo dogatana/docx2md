0362fefc5578135f361fbbd39f3644c528a93f48
Toshihiko Ichida <dogatana@gmail.com>
2021-08-11 17:38:46 +0900

    move strip_ns_prefix to utils.py

--- converter.py
+++ converter.py
@@ -5,23 +5,12 @@ import os.path
 
 from lxml import etree
 
-def strip_ns_prefix(tree):
-    # xpath query for selecting all element nodes in namespace
-    query = "descendant-or-self::*[namespace-uri()!='']"
-    # for each element returned by the above xpath query...
-    for element in tree.xpath(query):
-        # replace element name with its local name
-        element.tag = etree.QName(element).localname
-        # remove prefix from attribute name
-        stripped_attr = {re.sub(r"\{.*?\}", "", k): v for k, v in element.attrib.items()}
-        element.attrib.clear()
-        element.attrib.update(stripped_attr)
+import utils
 
 class Converter:
     def __init__(self, xml_text, resources, use_md_table=True):
         self.tree = etree.fromstring(xml_text)
-        strip_ns_prefix(self.tree)
-        self.resources = resources
+        utils.strip_ns_prefix(self.tree)
         self.image_counter = self.counter()
         self.table_counter = self.counter()
         self.use_md_table = use_md_table
