429d7e1478fa7e33e725d10b75e95cbcef8d8253
Toshihiko Ichida <dogatana@gmail.com>
2021-08-17 21:12:14 +0900

    update to 1.0.1

--- docx2md/__init__.py
+++ docx2md/__init__.py
@@ -2,6 +2,6 @@ from .docxfile import DocxFile, DocxFileError
 from .converter import Converter
 from .docxmedia import DocxMedia
 
-VERSION = (1, 0, 0)
+VERSION = (1, 0, 1)
 
 __version__ = ".".join(map(str, VERSION))
