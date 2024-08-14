dc915ae37cb9d58fd57eaecc778b61f06c2ea26d
Toshihiko Ichida <dogatana@gmail.com>
2024-08-14 11:06:16 +0900

    add interface

---
+++ docx2md/convert.py
@@ -0,0 +1,13 @@
+from .converter import Converter
+from .docxfile import DocxFile
+from .docxmedia import DocxMedia
+
+def do_convert(docx_file:str, use_md_table=False) -> str:
+    """convert docx_file to Markdown text and return it"""
+    try:
+        docx = DocxFile(docx_file)
+        media = DocxMedia(docx)
+        converter = Converter(docx.document(), media, use_md_table)
+        return converter.convert()
+    except Exception as e:
+        return f"Exception: {e}"
