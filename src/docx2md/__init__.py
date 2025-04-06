98ef85c31db13e7b1c1a08de4428144c889a9f12
Toshihiko Ichida <dogatana@gmail.com>
2025-04-07 07:00:24 +0900

    add API

--- src/docx2md/__init__.py
+++ src/docx2md/__init__.py
@@ -1,1 +1,6 @@
-# placeholder
+from .convert import do_convert  # noqa:F401
+from .converter import Converter  # noqa:F401
+from .docxfile import DocxFile  # noqa:F401
+from .docxmedia import DocxMedia  # noqa:F401
+
+all = ["do_convert", "Converter", "DocxFile", "DocxMedia"]
--- src/docx2md/convert.py
+++ src/docx2md/convert.py
@@ -3,11 +3,22 @@ from .docxfile import DocxFile
 from .docxmedia import DocxMedia
 
 
-def do_convert(docx_file: str, use_md_table=False) -> str:
-    """convert docx_file to Markdown text and return it"""
+def do_convert(docx_file: str, target_dir="", use_md_table=False)  -> str:
+    """
+    convert docx_file to Markdown text and return it
+    
+    Args:
+        docx_file(str): a file to parse
+        target_dir(str): save images of .docx if specified
+        use_md_table(bool): use Markdown table notation instead of HTHML
+    Returns:
+        Markdown text(str)
+    """
     try:
         docx = DocxFile(docx_file)
         media = DocxMedia(docx)
+        if target_dir:
+            media.save(target_dir)
         converter = Converter(docx.document(), media, use_md_table)
         return converter.convert()
     except Exception as e:
