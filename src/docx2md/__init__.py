d6f36ca0da3599579f4cc4468c1ace79e2e82a55
Toshihiko Ichida <dogatana@gmail.com>
2025-04-07 11:41:44 +0900

    export DocxFileError

--- src/docx2md/__init__.py
+++ src/docx2md/__init__.py
@@ -1,6 +1,6 @@
 from .convert import do_convert  # noqa:F401
 from .converter import Converter  # noqa:F401
-from .docxfile import DocxFile  # noqa:F401
+from .docxfile import DocxFile, DocxFileError  # noqa:F401
 from .docxmedia import DocxMedia  # noqa:F401
 
-all = ["do_convert", "Converter", "DocxFile", "DocxMedia"]
+all = ["do_convert", "Converter", "DocxFile", "DocxFileError", "DocxMedia"]
